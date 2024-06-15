d36829072e269659d4c9c673a04db6ca3896f23e
devil <devil14angel19@gmail.com>
2024-06-15 16:55:40 +0530

    loggedout page ready

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -148,3 +148,33 @@ const loginUser = asyncHandler(async(req,res) => {
     }
 })
 
+const logoutPage = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const option = {
+        httpOnly:true,
+        secure :true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", option)
+    .clearCookie("refreshToken",option)
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutPage,
+}
